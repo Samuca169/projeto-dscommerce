41359d7751c8b0a8d33e6760afb81ddce6a5bcc0
Samuca169 <samucabarr169@gmail.com>
2023-11-01 09:27:58 -0300

    Exception handling

---
+++ src/main/java/com/samucabarr/dscommerce/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.samucabarr.dscommerce.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/com/samucabarr/dscommerce/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.samucabarr.dscommerce.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
